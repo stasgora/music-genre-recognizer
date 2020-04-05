35288c3ada56a6269c96517b876c579a4d31b3c0
Mateusz <37753289+Evil00787@users.noreply.github.com>
2020-04-05 13:57:03 +0200

    Refactor

--- data-prep/data_splitter.py
+++ data-prep/data_splitter.py
@@ -29,35 +29,7 @@ def splitDataSet1(mainDir, k):  # main directory containing genres, number of se
 	print("Data has been splitted into set 1")
 	return finalSet
 
-
-def splitDataSet2(mainNormDir, k, lowestNumberOfFiles = 86):
-	j = 0
-	fileSplit = [[] for i in range(k)]
-	for genreDir in os.listdir(mainNormDir):
-		files = os.listdir(mainNormDir + slash + genreDir)
-		i = 0
-		while not len(files) == 0:
-			chosen = random.choice(files)
-			files.remove(chosen)
-			fileSplit[i].append(chosen)
-			i += 1
-			if i == k:
-				i = 0
-			j += 1
-			if j == lowestNumberOfFiles:
-				fileSplit[k-1].extend(files)
-				break
-	finalSet = [[] for i in range(3)]
-	finalSet[2] = fileSplit[k-1]
-	fileSplit.remove(finalSet[2])
-	finalSet[1] = random.choice(fileSplit)
-	fileSplit.remove(finalSet[1])
-	finalSet[0] = numpy.concatenate(fileSplit).tolist()
-	print("Data has been splitted into set 2")
-	return finalSet
-
-
-def splitDataSet3(mainNormDir, k, lowestNumberOfFiles=86):
+def uniformDatSplitter(mainNormDir, k, lowestNumberOfFiles):
 	j = 0
 	fileSplit = [[] for i in range(k)]
 	for genreDir in os.listdir(mainNormDir):
@@ -74,6 +46,22 @@ def splitDataSet3(mainNormDir, k, lowestNumberOfFiles=86):
 			if j == lowestNumberOfFiles:
 				fileSplit[k - 1].extend(files)
 				break
+	return fileSplit
+
+def splitDataSet2(mainNormDir, k, lowestNumberOfFiles = 86):
+	fileSplit = uniformDatSplitter(mainNormDir, k, lowestNumberOfFiles)
+	finalSet = [[] for i in range(3)]
+	finalSet[2] = fileSplit[k-1]
+	fileSplit.remove(finalSet[2])
+	finalSet[1] = random.choice(fileSplit)
+	fileSplit.remove(finalSet[1])
+	finalSet[0] = numpy.concatenate(fileSplit).tolist()
+	print("Data has been splitted into set 2")
+	return finalSet
+
+
+def splitDataSet3(mainNormDir, k, lowestNumberOfFiles=86):
+	fileSplit = uniformDatSplitter(mainNormDir, k, lowestNumberOfFiles)
 	finalSet = [[] for i in range(3)]
 	finalSet[2] = fileSplit[k - 1]
 	fileSplit.remove(finalSet[2])
