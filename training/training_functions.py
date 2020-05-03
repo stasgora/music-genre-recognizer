50e01dc37cb54720362414540027476cd0f626a5
Stanisław Góra <stasgora@gmail.com>
2020-05-03 18:23:38 +0200

    Training fixes

--- training/training_functions.py
+++ training/training_functions.py
@@ -1,8 +1,8 @@
-import librosa
 import numpy as np
 from tensorflow.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Dense, Flatten
 from tensorflow.python.keras.models import load_model
+
 from data_prep.data_splitter import *
 
 
@@ -16,7 +16,7 @@ def create_spectrograms(dataset):
 	for i in range(len(dataset)):
 		spectrograms[i] = np.load(dataset[i])
 		labels.append(genres.index(os.path.basename(dataset[i]).split('.')[-4]))
-	return spectrograms, labels
+	return spectrograms, np.array(labels)
 
 
 def load_dataset(split_index, normalized=True):
@@ -26,18 +26,21 @@ def load_dataset(split_index, normalized=True):
 
 
 def test_network(testing_set, testing_labels, network_path):
+	print('----TESTING----')
 	model = load_model(network_path)
 	model.evaluate(testing_set, testing_labels)
 
 
 def train_network(training_set, training_labels, save_path='network.keras'):
+	print('----TRAINING----')
 	model = Sequential([
-		Dense(256, input_dim=training_set[0].shape[0], activation='relu'),
+		Flatten(input_shape=training_set[0].shape),
+		Dense(256, activation='relu'),
 		Dense(64, activation='relu'),
 		Dense(10, activation='softmax'),
 	])
-	model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+	model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 	print(model.summary())
 
-	model.fit(training_set, training_labels, epochs=10, batch_size=64, validation_split=0)
+	model.fit(training_set, training_labels, epochs=30, batch_size=64, validation_split=0)
 	model.save(save_path)
