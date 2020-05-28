e65f105f9373498a058fb81afb3f6378baef291d
Stanisław Góra <stasgora@gmail.com>
2020-05-28 18:35:51 +0200

    Measures

--- training/training_functions.py
+++ training/training_functions.py
@@ -1,14 +1,18 @@
 import numpy as np
 import time
+
+import tensorflow.keras.backend as K
+from tensorflow.keras import metrics
 from tensorflow.keras.models import Sequential
+from tensorflow.python import confusion_matrix
 from tensorflow.python.keras.layers import Dense, Flatten
-from tensorflow.python.keras.models import load_model
+from tensorflow.python.keras.utils.np_utils import to_categorical
 
 from data_prep.data_splitter import *
 
-
 split_functions = [splitDataSet1, splitDataSet2, splitDataSet3]
 genres = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
+model = None
 
 
 def create_spectrograms(dataset):
@@ -17,7 +21,7 @@ def create_spectrograms(dataset):
 	for i in range(len(dataset)):
 		spectrograms[i] = np.load(dataset[i])
 		labels.append(genres.index(os.path.basename(dataset[i]).split('.')[-4]))
-	return spectrograms, np.array(labels)
+	return spectrograms, to_categorical(np.array(labels), num_classes=len(genres))
 
 
 def load_dataset(split_index, normalized=True):
@@ -27,8 +31,8 @@ def load_dataset(split_index, normalized=True):
 
 
 def test_network(testing_set, testing_labels, network_path):
+	global model
 	print('----TESTING----')
-	model = load_model(network_path)
 	train_time = time.time_ns()
 	history = model.evaluate(testing_set, testing_labels)
 	print((time.time_ns() - train_time) / 1000000)
@@ -37,6 +41,7 @@ def test_network(testing_set, testing_labels, network_path):
 
 
 def train_network(training_set, training_labels, save_path='network.keras'):
+	global model
 	print('----TRAINING----')
 	model = Sequential([
 		Flatten(input_shape=training_set[0].shape),
@@ -44,12 +49,50 @@ def train_network(training_set, training_labels, save_path='network.keras'):
 		Dense(64, activation='relu'),
 		Dense(10, activation='softmax'),
 	])
-	model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+	model_metrics = [metrics.CategoricalAccuracy(), metrics.Recall(), metrics.AUC(),
+	                 metrics.SensitivityAtSpecificity(.8), metrics.SpecificityAtSensitivity(.8), f1_score, fbeta_score]
+	model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=model_metrics)
 	print(model.summary())
 
 	train_time = time.time_ns()
-	history = model.fit(training_set, training_labels, epochs=15, batch_size=64, validation_split=0)
+	history = model.fit(training_set, training_labels, epochs=2, batch_size=32, validation_split=0)
 	print((time.time_ns() - train_time) / 1000000)
 	print(str(history.history['loss'])[1:-1].replace(',', ''))
-	print(str(history.history['accuracy'])[1:-1].replace(',', ''))
+	print(str(history.history['categorical_accuracy'])[1:-1].replace(',', ''))
+
 	model.save(save_path)
+
+def f1_score(y_true, y_pred):
+	precision = precision_m(y_true, y_pred)
+	recall = recall_m(y_true, y_pred)
+	return 2*((precision*recall) / (precision + recall + K.epsilon()))
+
+
+def fbeta_score(y_true, y_pred, threshold_shift=0):
+	beta = 2
+	y_pred = K.clip(y_pred, 0, 1)
+	y_pred_bin = K.round(y_pred + threshold_shift)
+
+	tp = K.sum(K.round(y_true * y_pred_bin)) + K.epsilon()
+	fp = K.sum(K.round(K.clip(y_pred_bin - y_true, 0, 1)))
+	fn = K.sum(K.round(K.clip(y_true - y_pred, 0, 1)))
+
+	precision = tp / (tp + fp)
+	recall = tp / (tp + fn)
+
+	beta_squared = beta ** 2
+	return (beta_squared + 1) * (precision * recall) / (beta_squared * precision + recall)
+
+
+def recall_m(y_true, y_pred):
+	true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+	possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+	recall = true_positives / (possible_positives + K.epsilon())
+	return recall
+
+
+def precision_m(y_true, y_pred):
+	true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+	predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+	precision = true_positives / (predicted_positives + K.epsilon())
+	return precision
